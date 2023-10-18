f6f78e6113af7a7d8de3366b94b4f65b927bbb51
RinaBoni <brisovae@mail.ru>
2023-10-18 21:33:29 +0900

    начало 2 лаб

--- lab2_1.py
+++ lab2_1.py
@@ -17,15 +17,36 @@ G.add_edge('N', 'P', label=".")
 G.add_edge('S', 'S', label="0, 1")
 G.add_edge('P', 'S', label="0, 1")
 
-# nx.draw_spring(G, with_labels=True)
+# nx.draw_spring(G, with_labels=True
 
+# Определяем координаты узлов для лучшей видимости
+pos = {
+    "H": (0, 0),
+    "S": (1, 0),
+    "P": (2, 0),
+    "N": (3, 0)
+}
 
-pos = nx.spring_layout(G, seed=42)  # Define layout
-nx.draw_networkx(G, pos, with_labels=True, node_size=50, node_color="skyblue", font_size=5, font_weight="bold")
+# Изменяем масштаб для увеличения расстояния между узлами
+pos = nx.spring_layout(G, pos=pos, fixed=pos.keys(), scale=2)
+
+# Рисуем граф
+nx.draw(G, pos, with_labels=True, node_size=1500, node_color="skyblue", font_size=10, font_weight="bold")
 edge_labels = nx.get_edge_attributes(G, 'label')
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8)
 
 plt.show()
+
+
+# # Изменяем масштаб для лучшей видимости
+# pos = nx.spring_layout(G, seed=42, scale=2.5)
+
+# # Рисуем граф
+# nx.draw_networkx(G, pos, with_labels=True, node_size=1500, node_color="skyblue", font_size=10, font_weight="bold")
+# edge_labels = nx.get_edge_attributes(G, 'label')
+# nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+# plt.show()
 
 
 
